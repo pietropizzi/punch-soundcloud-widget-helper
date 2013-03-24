c44b228bad01fc4b3a8dcded6bc60940d4583ae5
Peter Minarik <peter@peterminarik.com>
2013-03-24 17:14:32 +0100

    Start with implementation

--- spec/soundcloud-widget-helper.spec.js
+++ spec/soundcloud-widget-helper.spec.js
@@ -2,5 +2,11 @@ var soundcloud_widget_helper_obj = require('../src/soundcloud_widget_helper.js')
 var soundcloud_widget_helper = soundcloud_widget_helper_obj.directAccess()['block_helpers'];
 
 describe('behaviour of soundcloud widget helper', function() {
+  it('return nothing without arguments', function() {
+    
+  });
 
+  it('creates a user widget', function() {
+
+  });
 });
--- src/soundcloud-widget-helper.js
+++ src/soundcloud-widget-helper.js
@@ -1,13 +1,42 @@
-module.exports = {
 
-  get: function(basepath, file_extension, options, callback) {
-    var self = this;
+var soundCloudHelper,
+    _           = require('underscore'),
+    helperUtils = require('punch').Utils.Helper,
+    request     = require('request'),
 
-    return callback(null, { 'block': {} }, {});
-  },
+    blockHelpers = {
+      soundcloud: function() {
+        return helperUtils.checkArgs(arguments, function(text) {
+          var splitText, url, options = {},
+              apiResponse;
+
+          console.info('soundcloud-widget-helper.js: %o', text);
+
+          if (!text) {
+            return '';
+          }
+
+          splitText = text.split(' ');
+          url = splitText.shift();
+          _.each(splitText, function(textOption) {
+            var split = textOption.split('=');
+            options[split[0]] = split[1];
+          });
+
+          request('https://api.soundcloud.com/resolve.json?client_id=YOUR_CLIENT_ID&url=' + url, function(error, response, body) {
+            console.info('soundcloud-widget-helper.js: %o', body);
+          });
+        });
+      }
+    };
+
+soundCloudHelper = module.exports = {
 
   directAccess: function() {
-    return { 'block_helpers': {}, 'options': {} };
-  }
+    return { 'block_helpers': blockHelpers, 'options': {} };
+  },
 
+  get: function(basepath, fileExtension, options, callback) {
+    return callback(null, { 'block': blockHelpers }, {});
+  }
 };
